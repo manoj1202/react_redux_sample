05f44d2521c9c59a3f63d7befc06aacb917c9e42
manoj1202 <manoj.appalaneni@gmail.com>
2018-06-27 11:42:42 -0500

    react-redux

---
+++ src/actions/shared.js
@@ -0,0 +1,10 @@
+import {getInitialData} from "../utils/api"
+
+export function handleInitialData(){
+    return (dispatch) =>{
+        return getInitialData()
+         .then(({users, tweets}) => {
+             
+         })
+    }
+}
