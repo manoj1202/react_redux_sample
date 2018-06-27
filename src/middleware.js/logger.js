19be4a091502bfe0fd58573cecb65dcc382a255c
manoj1202 <manoj.appalaneni@gmail.com>
2018-06-27 16:46:50 -0500

    react-redux

---
+++ src/middleware.js/logger.js
@@ -0,0 +1,10 @@
+const logger =(store) => (next) => (action) {
+    console.group(action.type)
+        console.log("The action", action )
+        const returnValue = next(action)
+        console.log("The New state is", store.getState())
+    console.grounpEnd()
+    return returnValue
+}
+
+export default logger
