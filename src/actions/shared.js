c7ce58b13913c4a02a89fe7b07bf5d38376ece68
manoj1202 <manoj.appalaneni@gmail.com>
2018-06-27 12:02:52 -0500

    react-redux

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,9 @@
 import {getInitialData} from "../utils/api"
 import { receiveTweets } from "../actions/tweets";
-import {receiveUsers} from "../actions/users"
+import {receiveUsers} from "../actions/users";
+import {setAuthedUser} from "../actions/authedUsers";
+
+const AUTHED_ID = "tylermcginnis";
 
 export function handleInitialData(){
     return (dispatch) =>{
@@ -8,6 +11,7 @@ export function handleInitialData(){
          .then(({users, tweets}) => {
              dispatch(receiveUsers(users))
              dispatch(receiveTweets(tweets))
+             dispatch(setAuthedUser(AUTHED_ID))
          })
     }
 }
