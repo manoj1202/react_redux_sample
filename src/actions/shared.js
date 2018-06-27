4222c404ff655ae6aa255d81e3f53e7abd0321ab
manoj1202 <manoj.appalaneni@gmail.com>
2018-06-27 11:49:15 -0500

    react-redux

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,10 +1,13 @@
 import {getInitialData} from "../utils/api"
+import { receiveTweets } from "./tweets";
+import {receiveUsers} from "./users"
 
 export function handleInitialData(){
     return (dispatch) =>{
         return getInitialData()
          .then(({users, tweets}) => {
-             
+             dispatch(receiveUsers(users))
+             dispatch(receiveTweets(tweets))
          })
     }
 }
