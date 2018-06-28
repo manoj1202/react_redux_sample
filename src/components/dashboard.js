978db6d05d164ce82a5108de5e689157c975bd3f
manoj1202 <manoj.appalaneni@gmail.com>
2018-06-28 13:58:26 -0500

    react-redux

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,13 +1,22 @@
 import React,{ Component } from "react"
+import { connect } from "react-redux"
 
 class Dashboard extends Component{
     render(){
+        console.log(this.props)
         return(
             <div>
-              DashBoard
+              <h2>DashBoard</h2>
             </div>
         )
     }
 }
 
-export default Dashboard
+function mapStateTOProps({tweets}){
+    return{
+        tweetsIds: Object.keys(tweets)
+         .sort((a,b) => tweets[b].timestamp - tweets[a].timestamp )
+    }
+}
+
+export default connect(mapStateTOProps)(Dashboard)
